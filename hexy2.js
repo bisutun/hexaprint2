33c2b18f4709e559dfd628b53d4956f6085f752d
KAG <k.gilhooley@gmail.com>
2014-06-15 16:56:07 -0700

    + Ozzy mode

--- hexy2.js
+++ hexy2.js
@@ -3,15 +3,17 @@ function printMultiples(c, b, a) {
     a = parseInt(document.getElementById("mult").value);
     b = parseInt(document.getElementById("bound").value);
     c = parseInt(document.getElementById("start").value);
-    if ( c >= b ) {
-			document.getElementById("enumerated").innerHTML = ("That's not how ranges work!");
-	} else if ( (b - c) / a > 200 ) {
-			document.getElementById("enumerated").innerHTML = ("I'd rather not.");
-		} else { 
-			for ( var i = c; i <= b; i++ ) {
-			  if ( i % a === 0 ) {
-                document.getElementById("enumerated").innerHTML += ("<strong>" + i + "</strong>" + "<br/>");
-		      } 
-			}
-		} 
+	if (a === 6 && b === 6 && c === 6) {                                       // Ozzy mode
+		    document.getElementById("enumerated").innerHTML = ("Hail Satan.");
+        } else if ( c >= b ) {
+		    document.getElementById("enumerated").innerHTML = ("That's not how ranges work!");
+	} else if ( (b - c) / a > 200 ) {                                          // Bartleby mode
+		    document.getElementById("enumerated").innerHTML = ("I prefer not to.");
+        } else { 
+	        for ( var i = c; i <= b; i++ ) {
+                  if ( i % a === 0 ) {
+                    document.getElementById("enumerated").innerHTML += ("<strong>" + i + "</strong>" + "<br/>");
+		  } 
+		}
+	} 
 }
